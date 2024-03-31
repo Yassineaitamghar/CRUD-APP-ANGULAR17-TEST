63ec9f24f169b9dfa9dd579eacf743174cff8895
Yassineaitamhar <yassinaitmghar80l@gmail.com>
2024-03-31 21:21:15 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IndexComponent } from './post/index/index.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,IndexComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/post/index/index.component.ts
@@ -0,0 +1,45 @@
+import { CommonModule, NgFor } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Post } from '../post';
+import { PostService } from '../post.service';
+import { NgForOf } from '@angular/common';
+
+@Component({
+  selector: 'app-index',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './index.component.html',
+  styleUrl: './index.component.css'
+})
+export class IndexComponent {
+
+
+  posts: Post[] = [];
+
+  constructor(public postService: PostService) { }
+
+
+  ngOnInit(): void {
+    this.postService.getAll().subscribe((data: Post[]) => {
+      this.posts = data;
+      console.log(this.posts);
+    })
+
+  }
+
+  deletePost(id: number) {
+
+    this.postService.delete(id).subscribe(res => {
+      this.posts = this.posts.filter(item => item.id !== id);
+      alert("Post Deleted Succefully")
+    })
+
+  }
+
+
+
+
+
+
+}
